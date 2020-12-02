0308229b03124e2c164f5ccf4aef47b51c15fd02
Rowan <rojacobs@pivotal.io>
2020-12-01 20:56:09 -0800

    Do some very basic deduping

--- semshifter.py
+++ semshifter.py
@@ -1,5 +1,7 @@
 import csd, pollex, prototai, clics, stedt
 
+
 def semshift(search_term):
-    return csd.semshift(search_term) + pollex.semshift(search_term) + prototai.semshift(search_term) + \
-           clics.semshift(search_term) + stedt.semshift(search_term)
+    meanings = csd.semshift(search_term) + pollex.semshift(search_term) + prototai.semshift(search_term) + \
+               clics.semshift(search_term) + stedt.semshift(search_term)
+    return list(set(map(str.lower, meanings)))
