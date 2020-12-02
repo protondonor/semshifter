5ec66a211e9568ecc12cdc0a6249f027676485d3
Rowan <rojacobs@pivotal.io>
2020-12-01 20:25:33 -0800

    async requests for STEDT

--- stedt.py
+++ stedt.py
@@ -1,5 +1,7 @@
 import requests, json
 from lxml import html
+from requests_html import AsyncHTMLSession
+
 
 def semshift(search_term):
     r = requests.get(
@@ -8,11 +10,26 @@ def semshift(search_term):
     meanings = []
     try:
         ids = [item[0] for item in json.loads(r.content)['data']]
+
+        if len(ids) == 0:
+            return []
         urls = [f'https://stedt.berkeley.edu/~stedt-cgi/rootcanal.pl/etymon/{id}' for id in ids]
+        session = AsyncHTMLSession()
+
+        scrape_fns = []
         for url in urls:
-            r2 = requests.get(url)
+            async def get_site_content(url=url):
+                return await session.get(url)
+
+            scrape_fns.append(get_site_content)
+
+        results = session.run(*scrape_fns)
+
+        for r2 in results:
             tree2 = html.fromstring(r2.content)
             meanings += tree2.xpath('/html/body/table[2]/tbody/tr/td[5]/text()')
+
+        session.close()
     except json.decoder.JSONDecodeError:
         pass
 
