516f97772559120f0eedc8c14c3896e56c3b9962
Rowan <rojacobs@pivotal.io>
2020-12-06 08:25:10 -0800

    I don't know what I was thinking

--- semshifter.py
+++ semshifter.py
@@ -2,6 +2,7 @@ import csd, pollex, prototai, clics, stedt, datsemshift
 
 
 def semshift(search_term):
+    dss = datsemshift.DatSemShift()
     meanings = csd.semshift(search_term) + pollex.semshift(search_term) + prototai.semshift(search_term) + \
-               clics.semshift(search_term) + stedt.semshift(search_term) + stedt.semshift(datsemshift)
+               clics.semshift(search_term) + stedt.semshift(search_term) + dss.semshift(search_term)
     return list(set(map(str.lower, meanings)))
