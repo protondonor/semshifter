08ee28e155b6cf3c0e1a5c8ab66034c93ecfd2f8
Rowan <rojacobs@pivotal.io>
2020-12-22 20:04:11 -0800

    Enable tracing which dict a semshift came from

--- semshifter.py
+++ semshifter.py
@@ -4,15 +4,31 @@ from helper import clean_shift
 dss = datsemshift.DatSemShift()
 
 
-def semshift(search_term):
-    meanings = csd.semshift(search_term) + pollex.semshift(search_term) + prototai.semshift(search_term) + \
-               clics.semshift(search_term) + stedt.semshift(search_term) + dss.semshift(search_term)
-    return clean_shift(set(map(str.lower, meanings)))
+def semshift(search_term, dictionaries=None, trace=False):
+    if dictionaries is None:
+        dictionaries = ['csd', 'pollex', 'prototai', 'clics', 'stedt', 'dss']
+    meanings = {}
+    if 'csd' in dictionaries:
+        meanings['csd'] = clean_shift(map(str.lower, csd.semshift(search_term)))
+    if 'pollex' in dictionaries:
+        meanings['pollex'] = clean_shift(map(str.lower, pollex.semshift(search_term)))
+    if 'stedt' in dictionaries:
+        meanings['prototai'] = clean_shift(map(str.lower, stedt.semshift(search_term)))
+    if 'clics' in dictionaries:
+        meanings['clics'] = clean_shift(map(str.lower, clics.semshift(search_term)))
+    if 'dss' in dictionaries:
+        meanings['dss'] = clean_shift(map(str.lower, dss.semshift(search_term)))
+    if 'prototai' in dictionaries:
+        meanings['prototai']= clean_shift(map(str.lower, prototai.semshift(search_term)))
+    if trace:
+        return meanings
+
+    return {x for v in meanings.values() for x in v}
 
 
 def reverse(search_term, dictionaries=None):
     if dictionaries is None:
-        dictionaries = ['csd', 'pollex', 'stedt', 'clics']
+        dictionaries = ['csd', 'pollex', 'stedt', 'clics', 'dss']
     meanings = []
     if 'csd' in dictionaries:
         meanings += csd.reverse(search_term)
@@ -22,5 +38,7 @@ def reverse(search_term, dictionaries=None):
         meanings += stedt.reverse(search_term)
     if 'clics' in dictionaries:
         meanings += clics.reverse(search_term)
+    if 'dss' in dictionaries:
+        meanings += dss.reverse(search_term)
 
     return clean_shift(set(map(str.lower, meanings)))
