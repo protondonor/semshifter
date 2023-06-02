b46bab629ffda5a3f38d00df8a06cfa24dee0400
rowan <proton.donor.17@gmail.com>
2023-06-02 11:17:51 -0700

    attempt to appease the capricious new setuptools gods once again

---
+++ semshifter/pollex.py
@@ -0,0 +1,40 @@
+import requests
+from lxml import html
+
+from helper import french
+
+def semshift(search_term, include_french=False):
+    try:
+        r = requests.get(f'https://pollex.shh.mpg.de/search/?query=%5Cb{search_term}%5Cb&field=protoform', verify=False)
+        tree = html.fromstring(r.content)
+        meanings = []
+        for elem in tree.xpath('/html/body/div/div[3]/table/tr/td/a'):
+            r2 = requests.get(f"https://pollex.shh.mpg.de{elem.attrib['href']}", verify=False)
+            tree2 = html.fromstring(r2.content)
+            for entry in tree2.xpath('//*[@id="content"]/table[2]/tr/td[3]/text()'):
+                if len(entry.strip()) > 0 and (include_french or not french(entry.strip())):
+                    meanings.append(entry.strip())
+        return meanings
+    except ConnectionError:
+        return []
+
+
+def reverse(search_term, include_french=False):
+    try:
+        r = requests.get(f'https://pollex.shh.mpg.de/search/?query=%5Cb{search_term}%5Cb&field=entry', verify=False)
+        tree = html.fromstring(r.content)
+        meanings = []
+        urls = set([f"https://pollex.shh.mpg.de{elem.attrib['href']}"
+                    for elem in tree.xpath("/html/body/div/div[3]/table/tr/td[2]/a")])
+        for url in urls:
+            r2 = requests.get(url, verify=False)
+            tree2 = html.fromstring(r2.content)
+            try:
+                entry = tree2.xpath('//*[@id="content"]/table[1]/tr[1]/td/text()')[0]
+                if len(entry.strip()) > 0 and (include_french or not french(entry.strip())):
+                    meanings.append(entry.strip())
+            except IndexError:
+                pass
+        return meanings
+    except ConnectionError:
+        return []
--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 from setuptools import setup
-setup(name='semshifter', version='0.0.7', pymodules=["semshifter.py"])
+setup(name='semshifter', version='0.0.8', pymodules=["semshifter.py"])
